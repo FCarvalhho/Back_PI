c80371719bd599838130f9d066005a33f0f3a979
FCarvalhho <felipeborgesmelo80@gmail.com>
2026-06-08 12:11:06 -0300

    metodo de pagamento implementado

--- src/main/java/com/senai/PI_mecado_preso/sales/web/CarrinhoController.java
+++ src/main/java/com/senai/PI_mecado_preso/sales/web/CarrinhoController.java
@@ -1,13 +1,11 @@
 package com.senai.PI_mecado_preso.sales.web;
 
+import com.senai.PI_mecado_preso.sales.api.dtos.CarrinhoResponseDTO;
 import com.senai.PI_mecado_preso.sales.api.dtos.ItemCarrinhoRequestDTO;
 import com.senai.PI_mecado_preso.sales.internal.service.CarrinhoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedidos/carrinho")
@@ -23,5 +21,11 @@ public class CarrinhoController {
     public ResponseEntity<Void> adicionarItem(@Valid @RequestBody ItemCarrinhoRequestDTO request) {
         service.adicionarProdutoAoCarrinho(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<CarrinhoResponseDTO> obterCarrinho() {
+        CarrinhoResponseDTO carrinho = service.mostrarCarrinho();
+        return ResponseEntity.ok(carrinho);
     }
 }
